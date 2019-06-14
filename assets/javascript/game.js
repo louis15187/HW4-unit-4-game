9ee44eb295e93e4a069c68ff26dddeac2238494b
Louis Malone <louismalone@Louiss-MacBook-Air.local>
2019-06-13 21:52:58 -0400

    Commit Already

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,21 @@
+//Waits until the HTML is loaded to run the function//
 $(document).ready(function() {
 
+    //images// 
     crystals = ['assets/images/red.jpg', 'assets/images/blue.jpg', 'assets/images/yellow.jpg', 'assets/images/green.png'];
 
+    //Counting of Wins and Loses//
     var counter = 0;
     var wins = 0;
     var losses = 0;
     $('#win').text(wins);
     $('#loss').text(losses);
 
+    //Resets new game//
     newCrystals();
     newGame();
 
+    //Inserts numbers under the crystals and console logs the 4 numbers from 1 to 12//
     function newCrystals() {
         var numbers = []
         while (numbers.length < 4) {
@@ -26,6 +31,7 @@ $(document).ready(function() {
         }
         console.log(numbers);
 
+        //Allows the crystals to appear on the page and take up attributes above//
         for (i = 0; i < numbers.length; i++) {
             var imageCrystal = $('<img>');
             imageCrystal.attr('data-num', numbers[i]);
@@ -38,23 +44,29 @@ $(document).ready(function() {
 
     function newGame() {
 
+        //Logs your score//
         counter = 0;
         $('#yourScore').text(counter);
 
+        //random number function appears//
         function randomIntFromInterval(min, max) {
             return Math.floor(Math.random() * (max - min + 1) + min);
         }
 
+        //Returns an integer between 19-190//
         var numberToGuess = randomIntFromInterval(19, 120);
 
+        //Number Value//
         $('.value').text(numberToGuess);
 
-
+        //When crystal is clicked numbers will add until the Number is reached//
         $('.crystalImage').on('click', function() {
             counter = counter + parseInt($(this).data('num'));
 
+            //Keeps in mind the max//
             $('#yourScore').text(counter);
 
+            //Logs your win on the page and console logs the win as well and resets new game//
             if (counter == numberToGuess) {
                 $('#status').text('You won!!!!');
                 wins++;
@@ -64,6 +76,7 @@ $(document).ready(function() {
                 newCrystals();
                 newGame();
 
+                //Logs your loss on the page and console logs the loss as well and resets new game//
             } else if (counter > numberToGuess) {
                 $('#status').text('You lost!')
                 losses++;
